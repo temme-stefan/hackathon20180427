7f5e06680d31505b8750390d05eff63b118824ab
schmitt-stefan <schmitt@objectcode.de>
2018-04-27 14:12:23 +0200

    vue working

--- vue/first-app/src/index.js
+++ vue/first-app/src/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './components/App.vue'
+import Vue from 'vue';
+import App from './components/App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
